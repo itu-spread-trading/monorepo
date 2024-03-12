b56409af169a68f04e865064570acb89e862acf0
asgarovf <fasgarov2002@gmail.com>
2024-03-12 15:47:51 +0300

    [sdk] feat: extends sdk interface

--- packages/spread-sdk/src/types/ISpreadSDK.ts
+++ packages/spread-sdk/src/types/ISpreadSDK.ts
@@ -2,6 +2,11 @@ import { ISpreadSDKOrderbookModule } from './ISpreadSDKOrderbookModule';
 import { ISpreadSDKSwapModule } from './ISpreadSDKSwapModule';
 
 export interface ISpreadSDK {
+    /**
+     * Modules
+     */
     orderbook: ISpreadSDKOrderbookModule;
     swap: ISpreadSDKSwapModule;
+
+    connect(address: string, privateKey: string): void;
 }
